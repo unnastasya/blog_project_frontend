818b98638b264df76bd10bafe2cf28c2694781f9
Anastasiia Prokopeva <anastasiiaprokopeva31@gmail.com>
2023-03-29 12:52:20 +0500

    add UserInfo Component

---
+++ src/components/UserInfo/UserInfo.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./UserInfo.css";
+
+interface UserInfoProps {
+	avatarUrl: string;
+	fullName: string;
+	additionalText: string;
+}
+
+export function UserInfo({
+	avatarUrl,
+	fullName,
+	additionalText,
+}: UserInfoProps) {
+	return (
+		<div className="userInfo_root">
+			<img className="userInfo_avatar" src={avatarUrl} alt={fullName} />
+			<div className="userInfo_userDetails">
+				<span className="userInfo_username">{fullName}</span>
+				<span className="userInfo_additional">{additionalText}</span>
+			</div>
+		</div>
+	);
+}
